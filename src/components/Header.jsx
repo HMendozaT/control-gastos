72217831ca69fe9988bbcfdaaee72ef4c42f8be7
Hector Mendoza <h.mendozt@gmail.com>
2022-06-03 12:23:17 -0600

    Creacion de primer pagina y header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Presupuesto from "./Presupuesto";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Control de Gastos</h1>
+      <Presupuesto />
+    </header>
+  );
+};
+
+export default Header;
