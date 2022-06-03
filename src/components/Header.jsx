b9e08ba57e00463e5994a1a057a34141b8493af6
Hector Mendoza <h.mendozt@gmail.com>
2022-06-03 16:04:31 -0600

    Validacion de presupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,25 @@ import React from "react";
 
 import Presupuesto from "./Presupuesto";
 
-const Header = () => {
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   return (
     <header>
       <h1>Control de Gastos</h1>
-      <Presupuesto />
+
+      {isValidPresupuesto ? (
+        <p>Control Presupuesto</p>
+      ) : (
+        <Presupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
     </header>
   );
 };
